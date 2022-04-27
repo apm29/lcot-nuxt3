2453c77b0046cbea48666c21e5228d8e8e80010a
apm29 <835170486@qq.com>
2022-04-27 13:15:03 +0800

    chore: init

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <NuxtPage />
+</template>
+<script setup>
+useHead({
+  titleTemplate: "Nuxt.js - %s", // or, title => `My App - ${title}`
+  viewport: "width=device-width, initial-scale=1, maximum-scale=1",
+  charset: "utf-8",
+  meta: [{ name: "description", content: "My amazing site." }],
+  bodyAttrs: {
+    class: "app",
+  },
+});
+</script>
---
+++ pages/user.vue
@@ -0,0 +1,1 @@
+<template>NUXT USER</template>
